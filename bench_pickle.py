9c6e421f76e83d3048f94c8a171dc4ceb73776e3
Alex Willmer <alex@moreati.org.uk>
2018-04-27 07:56:14 +0100

    Bench cpickle.loads(float|int|bytes|unicode) on CPython 2.4-2.7, 3.3-3.6

---
+++ bench_pickle.py
@@ -0,0 +1,52 @@
+try:
+    import cPickle as cpickle
+except ImportError:
+    import pickle as cpickle
+
+import perf
+
+PROTOCOLS = [0, 2]
+
+runner = perf.Runner()
+
+# TODO pyppickle
+
+LENGTHS = [0, 1, 10, 100, 1000, 10000, 100000, 1000000]
+for protocol in PROTOCOLS:
+    for zeros in LENGTHS:
+        if protocol == 0:
+            # A pathological pickle that Python wouldn't normally create,
+            # but that the protocol allows
+            s = ('F1.'+'0'*zeros+'\n.').encode('ascii')
+        else:
+            s = cpickle.dumps(float('1.'+'0'*zeros), protocol=protocol)
+        runner.timeit(
+            'loads,float,protocol=%i,len=%i' % (protocol, zeros+1),
+            'loads(s)',
+            globals={'loads': cpickle.loads, 's': s},
+        )
+    for exponent in LENGTHS:
+        s = cpickle.dumps(10**exponent, protocol=protocol)
+        runner.timeit(
+            'loads,int,protocol=%i,len=%i' % (protocol, exponent+1),
+            'loads(s)',
+            globals={ 'loads': cpickle.loads, 's': s},
+        )
+
+    for n in LENGTHS:
+        s = cpickle.dumps(('s'*n).encode('ascii'), protocol=protocol)
+        runner.timeit(
+            'loads,bytes,protocol=%i,len=%i' % (protocol, n),
+            'loads(s)',
+            globals={ 'loads': cpickle.loads, 's': s},
+        )
+
+    for n in LENGTHS:
+        s = cpickle.dumps(u's'*n, protocol=protocol)
+        runner.timeit(
+            'loads,unicode,protocol=%i,len=%i' % (protocol, n),
+            'loads(s)',
+            globals={ 'loads': cpickle.loads, 's': s},
+        )
+
+
---
+++ bench_pickle_24.py
@@ -0,0 +1,91 @@
+import cPickle as cpickle
+
+import timeit
+
+PROTOCOLS = [0, 2]
+
+# TODO pyppickle
+
+def repeat(setup='pass', stmt='pass', repeat=timeit.default_repeat):
+    precision = 3
+    t = timeit.Timer(stmt, setup)
+    # determine number so that 2 <= total time < 20
+    for i in range(1, 10):
+        number = 10**i
+        try:
+            x = t.timeit(number)
+        except:
+            t.print_exc()
+            sys.exit(1)
+        if x >= 2:
+            break
+    try:
+        r = t.repeat(repeat, number)
+    except:
+        t.print_exc()
+        sys.exit(1)
+
+    return [(t, number) for t in sorted(r)]
+
+
+def eng(seconds, number):
+    nsec = seconds * 1e9 / number
+    if nsec < 1000: return nsec, 'ns'
+    usec = nsec / 1000
+    if usec < 1000: return usec, 'us'
+    msec = usec / 1000
+    if msec < 1000: return msec, 'ms'
+    return seconds, 's'
+
+
+LENGTHS = [0, 1, 10, 100, 1000, 10000, 100000, 1000000]
+for protocol in PROTOCOLS:
+    for zeros in LENGTHS:
+        if protocol == 0:
+            # A pathological pickle that Python wouldn't normally create,
+            # but that the protocol allows
+            s = ('F1.'+'0'*zeros+'\n.').encode('ascii')
+        else:
+            s = cpickle.dumps(float('1.'+'0'*zeros), protocol=protocol)
+        print 'loads,float,protocol=%i,len=%i' % (protocol, zeros+1)
+        times = repeat(
+            setup='from cPickle import loads; s=%r' % s,
+            stmt='loads(s)',
+            repeat=5,
+        )
+        print ', '.join('%.3f %s' % eng(*r) for r in sorted(times))
+        print times
+
+    for exponent in LENGTHS:
+        s = cpickle.dumps(10**exponent, protocol=protocol)
+        print 'loads,int,protocol=%i,len=%i' % (protocol, exponent+1)
+        times = repeat(
+            setup='from cPickle import loads; s=%r' % s,
+            stmt='loads(s)',
+            repeat=5,
+        )
+        print ', '.join('%.3f %s' % eng(*r) for r in sorted(times))
+        print times
+
+    for n in LENGTHS:
+        s = cpickle.dumps(('s'*n).encode('ascii'), protocol=protocol)
+        print 'loads,bytes,protocol=%i,len=%i' % (protocol, n)
+        times = repeat(
+            setup='from cPickle import loads; s=%r' % s,
+            stmt='loads(s)',
+            repeat=5,
+        )
+        print ', '.join('%.3f %s' % eng(*r) for r in sorted(times))
+        print times
+
+    for n in LENGTHS:
+        s = cpickle.dumps(u's'*n, protocol=protocol)
+        print 'loads,unicode,protocol=%i,len=%i' % (protocol, n)
+        times = repeat(
+            setup='from cPickle import loads; s=%r' % s,
+            stmt='loads(s)',
+            repeat=5,
+        )
+        print ', '.join('%.3f %s' % eng(*r) for r in sorted(times))
+        print times
+
