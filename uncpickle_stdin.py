26def7093187f31e85482c339bed4e3e2d479c21
Alex Willmer <alex@moreati.org.uk>
2018-07-28 13:27:25 +0100

    Wrap Unpickler creation and use in main()
    Improves python-afl instrumentation. AFL exec speed increased from 100/s
    to 750/s.

--- uncpickle_stdin.py
+++ uncpickle_stdin.py
@@ -9,34 +9,36 @@ class RestrictedUnpickler(pickle.Unpickler):
     def find_class(self, module, name):
         raise pickle.UnpicklingError('You shall not pass!')
 
-unpickler = RestrictedUnpickler(sys.stdin.buffer)
-try:
-    o = unpickler.load()
-except pickle.UnpicklingError:
-    pass
-except AttributeError:
-    pass
-except EOFError:
-    pass
-except ImportError:
-    pass
-except IndexError:
-    pass
-except KeyError:
-    pass
-except MemoryError:
-    pass
-except NameError:
-    pass
-except OverflowError:
-    pass
-except struct.error:
-    pass
-except SyntaxError:
-    pass
-except TypeError:
-    pass
-except UnicodeError:
-    pass
-except ValueError:
-    pass
+
+def main():
+    unpickler = RestrictedUnpickler(sys.stdin.buffer)
+    try:
+        o = unpickler.load()
+    except pickle.UnpicklingError:
+        pass
+    except AttributeError:
+        pass
+    except EOFError:
+        pass
+    except ImportError:
+        pass
+    except IndexError:
+        pass
+    except KeyError:
+        pass
+    except MemoryError:
+        pass
+    except NameError:
+        pass
+    except OverflowError:
+        pass
+    except struct.error:
+        pass
+    except SyntaxError:
+        pass
+    except TypeError:
+        pass
+    except UnicodeError:
+        pass
+    except ValueError:
+        pass
